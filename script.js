697c85f98ee8bc106c03cd1778e4fb5d5260874f
Arunpatwa123 <arunpatwa2605@gmail.com>
2024-03-04 17:19:48 +0530

    adding javascript

---
+++ script.js
@@ -0,0 +1,82 @@
+var  portfolio={
+    name:"Arun",
+    about_me:"Software Devloper",
+    Contact_Me:9138264825
+  }
+  console.log(portfolio);
+  console.log(typeof(name));
+  console.log(typeof(portfolio));
+  // Operators Arithmetic Operators
+  // Assignment Operators
+  // Comparison Operators
+  // String Operators
+  // Logical Operators
+  // Bitwise Operators
+  // Ternary Operators
+  // Type Operators
+  
+  
+  // Operators Arithmetic Operators
+  console.log("Arithmetic Operators")
+  var value1=10;
+  var value2=20;
+  console.log(value1+value2);
+  console.log(value1-value2);
+  console.log(value1*value2);
+  console.log(value1/value2);
+  console.log(value1%value2);
+  console.log(value1++);
+  console.log(++value1);
+
+// Assignment Operators
+console.log("Assignment Operators")
+var x=10;
+var y=x;
+console.log(y);
+  x += 5;
+  console.log(x);
+  x-=5;
+  console.log(x);
+  x*=4;
+  console.log(x);
+  x/=2;
+  console.log(x);
+  x%=2;
+  console.log(x);
+  x=10;
+  x**=2;
+  console.log(x);
+
+  console.log("Comparison Operators");
+  var l=10;
+  var m=20;
+  console.log(l==m);
+  console.log(l!=m);
+  console.log(l>m);
+  console.log(l<m);
+  console.log(l<=m);
+  console.log(l>=m);
+
+  console.log("String Operators");
+  console.log(l+" is less then "+m);
+
+console.log("Logical Operators"); 
+// And &&
+// or ||
+// Not !
+
+// function
+function functionName() {
+    console.log("Mera Naam Arun Hai");
+    
+}
+// var a;
+// console.log(a.functionName());
+functionName();
+function studentName(Name,Marks) {
+  console.log("Student Name is :"+Name);
+  console,log("Students Marks is: "+Marks)
+  
+}
+studentName("Arun",300);
+studentName("Khushi",400);
